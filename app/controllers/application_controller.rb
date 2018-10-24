73fd2bdc1484f4c4c2aaf3c53359b5a8d1917e3f
3415 <3415@example.com>
2018-10-24 11:54:12 +0900

    ログイン機能完成（仮）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def user_logged_in?
+    if session[:login] == 'ok'
+        return
+    end
+    session[:referer] = request.fullpath
+    redirect_to login_path
+end    
+
+def logout
+  session[:login] = nil
+end
+
 end
